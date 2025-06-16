97ec3eab9bfc292be388d71f7349d25c63047db6
Jesus Angel Quezada Camacho <jesusangelquezada@MacBook-Pro-de-Jesus.local>
2025-06-16 10:01:09 -0600

    Proyecto ejecutable

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,25 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
-module.exports = {
-  testEnvironment: "node",
+// jest.config.ts
+// @ts-check
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   transform: {
-    ...tsJestTransformCfg,
+    '^.+\\.ts$': 'ts-jest',
   },
+  moduleFileExtensions: ['ts', 'js'],
+  rootDir: '.',
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+
+  // Configurar alias según paths de tsconfig.json
+  moduleNameMapper: {
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@api-gateway/(.*)$': '<rootDir>/src/api-gateway/$1',
+    '^@validators/(.*)$': '<rootDir>/src/validators/$1',
+  },
+
+  // Asegurar que Jest encuentra los módulos en src y node_modules
+  moduleDirectories: ['node_modules', 'src'],
 };
+
+export default config;
